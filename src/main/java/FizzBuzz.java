755067d1dc27ba592ffb99a21bbcc7e91e66ab36
Hu Mengting <hmt9514@163.com>
2020-01-15 14:16:28 +0800

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,14 +2,14 @@
 public class FizzBuzz {
 
     public static String of(int num) {
-        if(isDivided(num, 3) && isDivided(num, 5)){
+        if (isDivided(num, 3) && isDivided(num, 5)) {
             return "FizzBuzz";
         }
 
-        if(isDivided(num, 3)){
+        if (isDivided(num, 3)) {
             return "Fizz";
         }
-        if(isDivided(num, 5)){
+        if (isDivided(num, 5)) {
             return "Buzz";
         }
         return String.valueOf(num);
