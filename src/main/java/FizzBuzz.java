4873d47f376c7577c945179bca5685e25d65743c
Hu Mengting <hmt9514@163.com>
2020-01-15 12:56:23 +0800

    Create FizzBuzz.java

---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,21 @@
+
+public class FizzBuzz {
+
+    public static String of(int num) {
+        if(isDivided(num, 3) && isDivided(num, 5)){
+            return "FizzBuzz";
+        }
+
+        if(isDivided(num, 3)){
+            return "Fizz";
+        }
+        if(isDivided(num, 5)){
+            return "Buzz";
+        }
+        return String.valueOf(num);
+    }
+
+    private static boolean isDivided(int num, int i) {
+        return num % i == 0;
+    }
+}
